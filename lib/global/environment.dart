7065c9da35bf1458c29ebbd8a738d0e2d39500e4
Giaeulate <giaeulate@gmail.com>
2022-08-01 01:37:29 -0400

    url modified

--- lib/global/environment.dart
+++ lib/global/environment.dart
@@ -3,6 +3,9 @@ class Environment {
   static double mobileSize = 500;
   static double desktopSize = 1200;
 
-  static String url = "http://192.168.1.14:4000";
-  static String socketUserUrl = "http://192.168.1.14:4000";
+  // static String url = "http://192.168.1.14:4000";
+  // static String socketUserUrl = "http://192.168.1.14:4000";
+
+  static String url = "https://tres.astronautas.gisdeb.com";
+  static String socketUserUrl = "https://tres.astronautas.gisdeb.com";
 }
